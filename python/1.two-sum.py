e53f470f46b25669ef73d938e441b1345f947f4c
“shota-08” <shota.kikko.bg@ambl.co.jp>
2025-05-07 22:09:46 +0900

    [1]Two Sum: advanced

--- python/1.two-sum.py
+++ python/1.two-sum.py
@@ -1,6 +1,6 @@
 # [1] Two Sum
 
-
+# O(n**2)
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         n = len(nums)
@@ -8,4 +8,17 @@ class Solution:
             for j in range(i+1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+        return []
+
+# O(n)
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        hash = {}
+        for i in range(len(nums)):
+            if nums[i] not in hash:
+                hash[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hash and hash[complement] != i:
+                return [i, hash[complement]]
         return []
