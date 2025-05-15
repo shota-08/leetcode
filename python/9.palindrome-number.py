42e6032ef850dcdb68665a0e416a08969f9df597
shota-08 <kame.s.0828@icloud.com>
2025-05-15 20:22:19 +0900

    [9] Palindrome Number: advanced

--- python/9.palindrome-number.py
+++ python/9.palindrome-number.py
@@ -1,5 +1,6 @@
 # [9] Palindrome Number
 
+# O(n)
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
@@ -9,3 +10,15 @@ class Solution:
         for i in range(len(str_x)-1, -1, -1):
             temp += str_x[i]
         return str_x == temp
+
+# O(n/2)
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        str_x = str(x)
+        n = len(str_x)
+        for i in range(n//2):
+            if str_x[i] != str_x[-(i+1)]:
+                return False
+        return True
