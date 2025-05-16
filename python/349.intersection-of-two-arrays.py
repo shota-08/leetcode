d3e6938abd5917f1ea0bd6950bb739f29eebcbdf
shota-08 <kame.s.0828@icloud.com>
2025-05-16 17:04:02 +0900

    [349] Intersection of Two Arrays: advanced

--- python/349.intersection-of-two-arrays.py
+++ python/349.intersection-of-two-arrays.py
@@ -10,3 +10,19 @@ class Solution:
             if i in nums2 and i not in res:
                 res.append(i)
         return res
+
+# O(n+m)
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set()
+        set2 = set()
+        res = []
+        for i in nums1:
+            if i not in set1:
+                set1.add(i)
+        for i in nums2:
+            if i in set1 and i not in set2:
+                res.append(i)
+            if i not in set2:
+                set2.add(i)
+        return res
