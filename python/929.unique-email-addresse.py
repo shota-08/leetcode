4f1a833accb879211e85c7079173877f4710f6ed
shota-08 <kame.s.0828@icloud.com>
2025-05-25 19:41:12 +0900

    [929] Unique Email Addresse: add comment

--- python/929.unique-email-addresse.py
+++ python/929.unique-email-addresse.py
@@ -6,14 +6,17 @@ class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
         uniques = set()
         for email in emails:
+            # find "@"
             di = email.find("@")
             local = email[:di]
             domain = email[di:]
 
+            # find "+"
             pi = local.find("+")
             if pi != -1:
                 local = local[:pi]
 
+            # find "."
             local = local.replace(".", "")
 
             comp = local + domain
